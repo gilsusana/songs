50b6e899ba86dccfbdbb51cd99733117167e0620
@gilsusana <gilsusanaCIBAF5@gmail.com>
2022-08-23 13:21:10 +0200

    kata

--- src/songs.js
+++ src/songs.js
@@ -1,33 +1,43 @@
+const songs = require("./data");
+
 //Exercise 1: Get the array of all Artists.
 function getAllArtists(array){
-    let result = '???';
-    //console.log("Exercise 1 ->", result);
-    return result;
+    let allArtists = array.map (item=> item.artist)
+    return allArtists;
 };
 
 //Exercise 2: Get the songs of a certain artist
 function getSongsFromArtist(array, artist){
-    //Write your code here
+    let songsArtist = array.filter(item => item.artist === artist);
+    return songsArtist;
 };
 
 //Exercise 3: Alphabetic order by title
-function orderAlphabetically(){
-    //Write your code here
-};
+function orderAlphabetically(array){
+    let order = array.map(item => item.title).sort().slice(0, 10);
+    return order;
+   };
 
 //Exercise 4: Order by year, ascending
-function orderByYear(){
-    //Write your code here
+function orderByYear(array){
+    let orderYear = array.map(item => ({ year:item.year, title:item.title})).sort((a,b) => a.year - b.year).sort((a,b) =>a.year === b.year ? a.title.localeCompare(b.title):null); 
+    return orderYear;
 };
 
 //Exercise 5: Filter songs by genre
-function songsByGenre() {
-    //Write your code here
+function songsByGenre(array, genre) {
+    let songsGenre= array.filter(item=> item.genre.includes(genre)); 
+    return songsGenre;
 };
 
 //Exercise 6: Modify the duration of songs to minutes
 function minutsToSeconds() {
-    //Write your code here
+let durationDivided = array.map(item => item.duration);
+let text="3min 30sec";
+let chars= text.split(" ");
+let minutes = parseInt(chars[0], 10);
+let seconds = parseInt(chars[1], 10);
+let number = minutes + seconds;   
 };
 
 //Exercise 7: Get the longest song
